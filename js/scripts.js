1cfd2ede361f3367b5833dc850f702244d98d99f
Łukasz Moszczyński <moszczynski.luk@gmail.com>
2018-01-17 09:55:41 +0100

    add dots to carousel

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 $(function(){
-	var carouselList = $("#carousel ul");
+	var carouselList = $("#carousel .images");
 	function moveFirstSlide() {
 		var firstItem = carouselList.find("li:first");
 		var lastItem = carouselList.find("li:last");
 		lastItem.after(firstItem);
-		carouselList.css({marginLeft: 0});
+		carouselList.css({marginLeft: -800});
 	}
 	function changeNextSlide() {
-		carouselList.animate({"marginLeft":-800}, 500, moveFirstSlide);
+		carouselList.animate({"marginLeft": -1600}, 500, moveFirstSlide);
 	}
 
 	var interval = setInterval(changeNextSlide, 3000);
@@ -16,21 +16,21 @@ $(function(){
 	function moveLastSlide() {
 		var firstItem = carouselList.find("li:first");
 		var lastItem = carouselList.find("li:last");
-		lastItem.before(firstItem);
-		carouselList.css({marginLeft: -800});
+		firstItem.before(lastItem);
+		carouselList.css({"marginLeft": -800});
 	}
 	function changePrevSlide() {
 		carouselList.animate({"marginLeft": 0}, 500, moveLastSlide);
 	}
 	$("#rightArrow").click(function(event) {
-		clearInterval(interval);
+	//	clearInterval(interval);
 		changeNextSlide();
-		interval = setInterval(changeNextSlide, 3000);
+	//	interval = setInterval(changeNextSlide, 3000);
 	});
 	$("#leftArrow").click(function(event) {	
-		clearInterval(interval);
+		//clearInterval(interval);
 		changePrevSlide();
-		interval = setInterval(changeNextSlide, 3000);
+		//interval = setInterval(changeNextSlide, 3000);
 	});
 	$("#carousel").mouseover(function(event) {
 		clearInterval(interval);
@@ -39,5 +39,29 @@ $(function(){
 		clearInterval(interval);
 		interval = setInterval(changeNextSlide, 3000);
 	});
+	var numberOfElements = carouselList.find("li").length;
+	for (var i=0; i < numberOfElements; i++) {
+		$("#carousel .dots").append('<li data-number="' + i + '"></li>');
+		$("#carousel .dots").find("li:first").addClass("active");
+	}
+	$("#carousel .dots li").click(function(event) {
+		var prevSlideNumber = $("#carousel .dots .active").attr('data-number');
+		$("#carousel .dots li").removeClass("active");
+		$(this).toggleClass("active");
+		var acctiveSlideNumber = $(this).attr('data-number');
+		if (acctiveSlideNumber > prevSlideNumber){
+			var change = acctiveSlideNumber - prevSlideNumber;
+			for (var j=0; j < change; j++){
+				changeNextSlide();
+			}
+		}
+		if (acctiveSlideNumber < prevSlideNumber){
+			var prev = prevSlideNumber - acctiveSlideNumber;
+			for (var k=0; k < prev; k++){
+				changePrevSlide();
+			}
+		}
+	});
+
 });
 
